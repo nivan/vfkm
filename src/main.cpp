70b0d1e71d55a09702b48c51bdadfedce3221857
Nivan <nivan.ferreira@gmail.com>
2013-06-23 10:01:10 -0700

    writing experiment output

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <cfloat>
 #include <cstdlib>
 #include <sstream>
+#include <queue>
+#include <map>
+#include <cassert>
 
 #include "Optimizer.h"
 #include "Util.h"
@@ -37,20 +40,92 @@ void initExperiment(string filename, Cluster*& rootCluster, Grid*& g,
     }
 }
 
+void saveExperiment(string directory, string currentFileLoaded, Cluster* root){
+    stringstream ss;
+    ss << directory << "/experiment.txt";
+
+    ofstream experimentFile(ss.str().c_str());
+    experimentFile << currentFileLoaded << endl;
+
+    //write hierarchy
+    queue<Cluster*> nodesToProcess;
+
+    nodesToProcess.push(root);
+
+    //hack
+    map<Cluster*, string> mapClusterPath;
+    mapClusterPath[root] = "r";
+
+    experimentFile << "-1 r " << endl;
+
+    while(!nodesToProcess.empty()){
+        Cluster* c = nodesToProcess.front();
+        nodesToProcess.pop();
+
+        string name = mapClusterPath[c];
+
+        //write curve indices file
+        stringstream curveFileName;
+        curveFileName << directory << "/curves_" << name << ".txt";
+        ofstream curveIndicesFile(curveFileName.str().c_str());
+        int numberOfCurves = c->indices.size();
+
+        //cout << "NumCurves " << numberOfCurves << " errors " << c->curveErrors.size() << endl;
+        assert(numberOfCurves == (int)c->curveErrors.size());
+
+        for(int i = 0 ; i < numberOfCurves ; ++i){
+            curveIndicesFile << c->indices.at(i) << " " << c->curveErrors.at(i) << endl;
+        }
+        curveIndicesFile.close();
+
+//write vector field file file
+        stringstream vectorFieldFileName;
+        vectorFieldFileName << directory << "/vf_" << name << ".txt";
+        ofstream vectorFieldFile(vectorFieldFileName.str().c_str());
+        Vector* xComponent = c->vectorField.first;
+        Vector* yComponent = c->vectorField.second;
+        int gridDimension = xComponent->getDimension();
+
+        vectorFieldFile << gridDimension << endl;
+
+        for(int i = 0 ; i < gridDimension ; ++i){
+            vectorFieldFile << xComponent[0][i] << " " << yComponent[0][i] << endl;
+        }
+        vectorFieldFile.close();
+
+        //process children
+        int numberOfChildren = c->children.size();
+
+        for(int i = 0 ; i < numberOfChildren ; ++i){
+            Cluster* child = c->children.at(i);
+
+            stringstream ss;
+            ss << name << "_" <<  i;
+            mapClusterPath[child] = ss.str();
+
+            experimentFile << name << " " << ss.str() << endl;
+
+            nodesToProcess.push(child);
+        }
+    }
+
+}
+
 int main(int argc, char *argv[]){
     int rightNumberOfParameters = 6;
 
     if(argc != rightNumberOfParameters){
         //print usage
-        cout << "./vfkm trajectoryFile gridResolution numberOfVectorFields smoothnessWeight" << endl;
+        cout << "./vfkm trajectoryFile gridResolution numberOfVectorFields smoothnessWeight outputDirectory" << endl;
         return 0;
     }
 
     //set parameters
     string filename(argv[1]);
     int    gridResolution = atoi(argv[2]);
-    int    maxNumberOfVectorFields = atoi(argv[3]);
+    int    numberOfVectorFields = atoi(argv[3]);
     float  smoothnessWeight = atof(argv[4]);
+    string outputDirectory(argv[5]);
 
     //load files
     cout << "Loading Files..." << endl;
@@ -58,77 +133,112 @@ int main(int argc, char *argv[]){
     Cluster* rootCluster = NULL;
     Grid* g = NULL;
 
-    //for now always project
+    //
     cout << "Loading data" << endl;
     initExperiment(filename, rootCluster, g, curves, gridResolution);
 
     //optimize
     cout << "Optimizing..." << endl;
-    float totalErrorsPerExecution[maxNumberOfVectorFields];
-    for(int k = 1 ; k <= maxNumberOfVectorFields ; ++k){
-	//
-	Cluster* currentCluster = rootCluster;
+    Cluster* currentCluster = rootCluster;
 
-	int numberOfVectorFields = k;
+    //Optimize
+    Optimizer op(g->getResolutionX() * g->getResolutionY());
+    int numberOfCurves = currentCluster->indices.size();
 
+    unsigned short mapCurveToVF[numberOfCurves];
+    float mapCurveToError[numberOfCurves];
+    unsigned int mapCurveToIndexInCurveVector[numberOfCurves];
+    vector<float> mapVectorFieldToError;
+    vector<PolygonalPath> curvesInCurrentCluster;
 
-	//Optimize
-	cout << "Optimizing " << k << " vector fields" << endl;
-	Optimizer op(g->getResolutionX() * g->getResolutionY());
-	int numberOfCurves = currentCluster->indices.size();
-	unsigned short mapCurveToVF[numberOfCurves];
-	float mapCurveToError[numberOfCurves];
-	unsigned int mapCurveToIndexInCurveVector[numberOfCurves];
+    for(int i = 0 ; i <numberOfCurves ; ++i){
+        mapCurveToError[i] = 0;
+        mapCurveToVF[i] = -1;
 
-	vector<PolygonalPath> curvesInCurrentCluster;
-
-	for(int i = 0 ; i <numberOfCurves ; ++i){
-	    mapCurveToError[i] = 0;
-	    mapCurveToVF[i] = -1;	   
-	    curvesInCurrentCluster.push_back(curves.at(currentCluster->indices.at(i)));
-	    mapCurveToIndexInCurveVector[i] = currentCluster->indices.at(i);
-	}
-
-	//create vector fields
-	vector<pair<Vector*,Vector*> > vectorFields;
-	int gridDimension  = g->getResolutionX() * g->getResolutionY();
-	for(int i = 0 ; i < numberOfVectorFields ; ++i){
-	    Vector* xComponent = new Vector(gridDimension);
-	    Vector* yComponent = new Vector(gridDimension);
-	    vectorFields.push_back(make_pair(xComponent, yComponent));
-	}
-
-	op.optimizeImplicitFastWithWeights(*g,numberOfVectorFields, curvesInCurrentCluster,
-                                       vectorFields, &(mapCurveToVF[0]), mapCurveToError,smoothnessWeight);
-
-	double totalError = 0.0f;
-
-	for(int i = 0 ; i < numberOfCurves ; ++i){
-	    totalError += mapCurveToError[i];	    
-	}
-
-	totalErrorsPerExecution[k-1] = totalError;
-
-	//clean memory
-	cout << "Cleaning Memory" << endl;
-	for(int i = 0 ; i < numberOfVectorFields ; ++i){
-	    std::pair<Vector*, Vector*> &vfs = vectorFields.at(i);
-	    Vector* componentX  = vfs.first;
-	    if(componentX != NULL){
-		delete componentX;
-	    }
-	    
-	    Vector* componentY  = vfs.second;
-	    if(componentY != NULL){
-		delete componentY;
-	    }
-
-	}
+        curvesInCurrentCluster.push_back(curves.at(currentCluster->indices.at(i)));
+        mapCurveToIndexInCurveVector[i] = currentCluster->indices.at(i);
     }
 
-    cout << endl << "Error Per Execution" << endl;
-    for(int k = 1 ; k <= maxNumberOfVectorFields ; ++k){
-	cout << "   With " << k << " vfs error was " << totalErrorsPerExecution[k-1] << endl;
+    vector<pair<Vector*,Vector*> > vectorFields;
+    int gridDimension  = g->getResolutionX() * g->getResolutionY();
+    for(int i = 0 ; i < numberOfVectorFields ; ++i){
+        Vector* xComponent = new Vector(gridDimension);
+        Vector* yComponent = new Vector(gridDimension);
+        vectorFields.push_back(make_pair(xComponent, yComponent));
+    }
+
+    op.optimizeImplicitFastWithWeights(*g,numberOfVectorFields, curvesInCurrentCluster,
+                                       vectorFields, &(mapCurveToVF[0]), mapCurveToError,smoothnessWeight);
+
+    //count number of curves for each vf
+    mapVectorFieldToError = vector<float>(vectorFields.size(),0);
+    int numberOfChildren = vectorFields.size();
+    int numberOfCurvesPerVF[numberOfChildren];
+    for(int i = 0 ; i < numberOfChildren ; ++i){
+        numberOfCurvesPerVF[i] = 0;
+    }
+
+    //compute error by vector field and total error
+    vector<vector<int> > mapCurvesToClusters(numberOfChildren,vector<int>());
+    vector<vector<float> > mapCurveErrorsToClusters(numberOfChildren,vector<float>());
+
+
+    for(int i = 0 ; i < numberOfCurves ; ++i){
+        vector<int>& curveCluster
+                = mapCurvesToClusters.at(mapCurveToVF[i]);
+        vector<float>& curveErrorsCluster
+                = mapCurveErrorsToClusters.at(mapCurveToVF[i]);
+
+        curveCluster.push_back(mapCurveToIndexInCurveVector[i]);
+        curveErrorsCluster.push_back(mapCurveToError[i]);
+
+        float &error = mapVectorFieldToError.at(mapCurveToVF[i]);
+        error += mapCurveToError[i];
+        numberOfCurvesPerVF[mapCurveToVF[i]] += 1;
+    }
+
+    //update cluster struct
+    currentCluster->clearChildren();
+    for(int i = 0 ; i < numberOfChildren ; ++i){
+        Cluster* c = new Cluster();
+        c->children.clear();
+        c->parent = currentCluster;
+        stringstream ss;
+        ss << currentCluster->name << ":" << i;
+        c->name = ss.str();
+        c->error = mapVectorFieldToError[i];
+        c->indices = mapCurvesToClusters.at(i);
+        c->curveErrors = mapCurveErrorsToClusters.at(i);
+        currentCluster->children.push_back(c);
+
+        c->vectorField = vectorFields.at(i);
+
+        float maxE = -1000;
+        for(int j = 0 ; j < (int)c->curveErrors.size() ; ++j){
+            float currentError = c->curveErrors.at(j);
+            if(currentError > maxE)
+                maxE = currentError;
+        }
+        c->maxError = maxE;
+    }
+
+    //
+    saveExperiment(outputDirectory, filename, rootCluster);
+
+    //clean memory
+    cout << "Cleaning Memory" << endl;
+    for(int i = 0 ; i < numberOfVectorFields ; ++i){
+	std::pair<Vector*, Vector*> &vfs = vectorFields.at(i);
+	Vector* componentX  = vfs.first;
+	if(componentX != NULL){
+	    delete componentX;
+	}
+	    
+	Vector* componentY  = vfs.second;
+	if(componentY != NULL){
+	    delete componentY;
+	}
+
     }
 
 }
